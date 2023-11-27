44a00fda371d6ccb013fbdbb8b6b60b10e8512a8
Harshith Kumar <kharshith47@gmail.com>
2023-11-27 02:20:50 -0500

    added support for 'all' in query1

--- neo/query/all_queries.py
+++ neo/query/all_queries.py
@@ -1,13 +1,11 @@
 Industry_Gdp_Query = """
 SELECT 
     Curr.year as Year,Curr.iname as Industry_Name,
-    Curr.State_Name as State,
     Gdp_per_year.Total_Gdp as Total_Gdp,
     (Curr.Total_Annual_Pay - Prev.Total_Annual_Pay)/Prev.Total_Annual_Pay * 100 as Growth
     FROM
         (SELECT
             G1.year as year,
-            S.Name as State_Name,
             SUM(DISTINCT G1.real_gdp) as Total_Gdp
         FROM
             "HARSHITH.KUMAR".Gdp G1
@@ -19,14 +17,14 @@ SELECT
             "HARSHITH.KUMAR".State S
         ON
             CF1.state_fips = S.fips
+        WHERE S.Name = CASE WHEN '{state}' = 'all' THEN S.Name ELSE '{state}' END
         GROUP BY
-            G1.year, S.name
+            G1.year
         ) Gdp_per_year
     JOIN 
         (SELECT
             G1.year as year,
             IC1.industry_name as iname,
-            S1.Name as State_Name,
             SUM(G1.real_gdp) as Total_Gdp,
             SUM(I1.Annual_Pay) as Total_Annual_Pay
         FROM
@@ -48,20 +46,18 @@ SELECT
             "HARSHITH.KUMAR".Industry_Category IC1
         ON
             IC1.naics = I1.naics
-        WHERE S1.Name IN ({bind_states}) AND
+        WHERE S1.Name = CASE WHEN '{state}' = 'all' THEN S1.Name ELSE '{state}' END AND
         IC1.naics IN ({bind_industries}) AND
         I1.year >= {start_year} AND
         I1.year <= {end_year}
         GROUP BY
-            G1.year, IC1.Industry_Name, S1.name
+            G1.year, IC1.Industry_Name
     ) CURR
     ON Gdp_per_year.year = CURR.year
-    AND Gdp_per_year.State_Name = CURR.State_Name
     JOIN (
         SELECT 
             I2.year as year, IC2.industry_name as iname, 
-            SUM(I2.Annual_Pay) as Total_Annual_Pay,
-            S2.name as State_Name
+            SUM(I2.Annual_Pay) as Total_Annual_Pay
         FROM
             "HARSHITH.KUMAR".Industry I2
         JOIN
@@ -76,13 +72,13 @@ SELECT
             "HARSHITH.KUMAR".Industry_Category IC2
         ON
             I2.naics = IC2.naics
+        WHERE S2.Name = CASE WHEN '{state}' = 'all' THEN S2.Name ELSE '{state}' END
         GROUP BY
-            I2.year, IC2.industry_name, S2.name
+            I2.year, IC2.industry_name
         ) PREV
     ON 
         CURR.year = PREV.year+1 AND
-        CURR.iname = PREV.iname AND
-        CURR.State_Name = PREV.State_Name
+        CURR.iname = PREV.iname
 ORDER BY CURR.year ASC
 """
 
--- neo/query/query1.py
+++ neo/query/query1.py
@@ -9,35 +9,34 @@ bp = Blueprint('query1', __name__, url_prefix='/query1')
 
 @bp.route('', methods=['GET'])
 def fetch_industry_gdp_relation():
-    states = request.args.get('states')
+    state = request.args.get('state')
     industries = request.args.get('naics')
     start_year = request.args.get('start_year')
     end_year = request.args.get('end_year')
 
-    states_list = [state.strip() for state in states.split(',')] if states else []
     industries_list = [industry.strip() for industry in industries.split(',')] if industries else []
-    bind_states = ",".join(":" + str(i + 1) for i in range(len(states_list)))
-    bind_industries = ",".join(":" + str(len(states_list) + i + 1) for i in range(len(industries_list)))
-    q = Industry_Gdp_Query.format(bind_states=bind_states, bind_industries=bind_industries, start_year=start_year,
+    bind_industries = ",".join(":" + str(i + 1) for i in range(len(industries_list)))
+    q = Industry_Gdp_Query.format(state=state, bind_industries=bind_industries, start_year=start_year,
                                   end_year=end_year)
 
     conn = app.config['DB_CONN']
     cursor = conn.cursor()
 
-    cursor.execute(q, states_list + industries_list)
+    cursor.execute(q, industries_list)
     results = cursor.fetchall()
     cursor.close
+    # print(q)
     print(results)
 
     response_data = {}
     for result in results:
         year = result[0]
         industry = result[1]
-        gdp = result[3]
-        growth = result[4]
+        gdp = result[2]
+        growth = result[3]
 
         if year not in response_data:
-            response_data[year] = {"year": year, "gdp": result[3]}
+            response_data[year] = {"year": year, "gdp": gdp}
 
         response_data[year][industry] = growth
 
